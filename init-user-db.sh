9a32580e5a9918f7c2af8df486bfa18aa56e12d8
Leonid <mr.lenyaxxx@gmail.com>
2021-04-19 19:42:30 +0300

    edit docker

--- init-user-db.sh
+++ init-user-db.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER docker;
     CREATE DATABASE twitter;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+    GRANT ALL PRIVILEGES ON DATABASE twitter TO docker;
 EOSQL
